1ffce28278a2fd88aac3988478124ffc11d1f475
Esteban Dalel R <estebandalelr@gmail.com>
2018-05-09 16:32:23 -0500

    Got the react VR start to work

---
+++ finalProject/GerisGame/index.vr.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  VrButton,
+  Image
+} from 'react-vr';
+
+export default class GerisGame extends React.Component {
+  state = {
+    playing: true,
+  }
+
+  startPlaying = () => {
+    this.setState({ playing: false })
+  }
+
+  showHome() {
+    if (this.state.playing) {
+      return (
+        <VrButton
+          onClick={this.startPlaying}
+        >
+          <Text
+            style={{
+              backgroundColor: '#777879',
+              fontSize: 0.8,
+              fontWeight: '400',
+              layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{ translate: [0, 0, -3] }],
+            }}>
+            Geri's Game
+        </Text>
+          <Text
+            style={{
+              backgroundColor: '#777879',
+              fontSize: 0.8,
+              fontWeight: '400',
+              layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{ translate: [0, -1, -3] }],
+            }}>
+            Start
+        </Text>
+        </VrButton>
+      )
+    }
+    else {
+      return (
+        <View
+          style={{
+            layoutOrigin: [0.5, 0.5],
+            transform: [{ translate: [0, -1, -3] }],
+          }}
+        >
+          <Image
+            source={asset('chess.jpg')}
+            style={{width: 3, height: 3}}
+          />
+        </View>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Pano source={asset('park.jpg')} />
+        {this.showHome()}
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('GerisGame', () => GerisGame);
